a336e781478064156c348101508b9a8493736926
unknown <brandon.dsweat@gmail.com>
2026-06-02 19:06:57 -0700

    Update root main.tf: - Remove aws_eks_cluster + aws_eks_cluster_auth data sources (read at plan time, fail before cluster exists) - Switch kubernetes and helm providers to reference module outputs directly - Switch kubernetes provider auth from static token to exec (aws eks get-token)

--- tofu/main.tf
+++ tofu/main.tf
@@ -2,24 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "main" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = data.aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
